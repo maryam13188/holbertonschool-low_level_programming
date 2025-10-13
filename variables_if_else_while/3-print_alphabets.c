0e1007003cec1191e2e93bd757b02da9a5ac81bf
maryam13188 <roro13188@gmail.com>
2025-10-13 10:01:22 +0000

    Add 3-print_alphabets program

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, then uppercase
+ *
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	char c; /* variable to store each letter */
+
+	/* print lowercase letters from 'a' to 'z' */
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	/* print upper*
