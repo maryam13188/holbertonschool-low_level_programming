93ca5b49fc67766bfe5026bed6e8e4a0aa3fa4ec
maryam13188 <roro13188@gmail.com>
2025-10-13 10:25:19 +0000

    add

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers 0 to 9 separated by comma and space
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
