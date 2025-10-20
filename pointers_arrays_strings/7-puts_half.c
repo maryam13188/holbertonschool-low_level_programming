d4ddc47172fda26c229e828c615cfa2d4bd5b1c8
maryam13188 <roro13188@gmail.com>
2025-10-20 18:37:11 +0000

    add

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,21 +6,19 @@
  */
 void puts_half(char *str)
 {
-    int len = 0, i;
+	int len = 0;
+	int i;
 
-    /* calculate the length of the string */
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    /* determine the starting index of the second half */
-    i = (len % 2 == 0) ? (len / 2) : ((len + 1) / 2);
+	i = (len % 2 == 0) ? (len / 2) : ((len + 1) / 2);
 
-    /* print characters from the middle to the end */
-    while (i < len)
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
