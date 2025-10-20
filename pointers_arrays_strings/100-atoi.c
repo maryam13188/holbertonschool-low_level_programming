5f5631628de230b8137d4b69db138c1cab0c97ad
maryam13188 <roro13188@gmail.com>
2025-10-20 19:08:56 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -16,8 +16,23 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			/* accumulate number safely */
 			while (s[i] >= '0' && s[i] <= '9')
 			{
+				/* check potential overflow before multiplying */
+				if (num > 214748364)
+				{
+					num = (sign == 1) ? 2147483647 : -2147483648;
+					return (num);
+				}
+				else if (num == 214748364)
+				{
+					if ((s[i] - '0') > 7 && sign == 1)
+						return (2147483647);
+					if ((s[i] - '0') > 8 && sign == -1)
+						return (-2147483648);
+				}
+
 				num = num * 10 + (s[i] - '0');
 				i++;
 			}
