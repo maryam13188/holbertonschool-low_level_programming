eb3e019236a328e72b44d2d315f67fb662307d6c
maryam13188 <roro13188@gmail.com>
2025-10-28 07:45:52 +0000

    Add 1-args task

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv; /* Unused parameter */
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
