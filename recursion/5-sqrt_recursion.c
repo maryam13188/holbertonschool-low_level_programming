1093d5f69b700b948c8df71e8c8104793c433bc4
maryam13188 <roro13188@gmail.com>
2025-10-28 06:39:19 +0000

    Add 5-sqrt_recursion task

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,25 +3,27 @@
 /**
  * _sqrt_helper - helper function to find square root using binary search
  * @n: number to find square root of
- * @start: starting point for search
- * @end: ending point for search
+ * @low: lower bound
+ * @high: upper bound
  * Return: natural square root or -1 if not found
  */
-int _sqrt_helper(int n, int start, int end)
+int _sqrt_helper(int n, int low, int high)
 {
 	int mid;
+	long square;
 
-	if (start > end)
+	if (low > high)
 		return (-1);
 
-	mid = (start + end) / 2;
+	mid = low + (high - low) / 2;
+	square = (long)mid * mid;
 
-	if (mid * mid == n)
+	if (square == n)
 		return (mid);
-	else if (mid * mid > n)
-		return (_sqrt_helper(n, start, mid - 1));
+	else if (square < n)
+		return (_sqrt_helper(n, mid + 1, high));
 	else
-		return (_sqrt_helper(n, mid + 1, end));
+		return (_sqrt_helper(n, low, mid - 1));
 }
 
 /**
@@ -36,5 +38,5 @@ int _sqrt_recursion(int n)
 	if (n == 0 || n == 1)
 		return (n);
 
-	return (_sqrt_helper(n, 1, n));
+	return (_sqrt_helper(n, 1, 46340));
 }
