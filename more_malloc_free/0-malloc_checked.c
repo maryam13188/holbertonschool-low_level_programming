b542a56d44d9d44ba425a68693c9309c291cb304
maryam13188 <roro13188@gmail.com>
2025-11-04 08:58:00 +0000

    Task 0: malloc_checked - allocate memory with error exit(98)

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = malloc(b);
-    
-    if (ptr == NULL)
-        exit(98);
-    
-    return (ptr);
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
