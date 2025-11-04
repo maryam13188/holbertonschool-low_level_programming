7a25187093f0afb7f135a5d9fe036fb268c125c0
maryam13188 <roro13188@gmail.com>
2025-11-04 09:41:00 +0000

    add

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,143 +1,95 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
-/**
- * is_digit - checks if a string contains only digits
- * @s: string to check
- *
- * Return: 1 if all digits, 0 otherwise
- */
-int is_digit(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to check
- *
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * multiply - multiplies two positive numbers
- * @s1: first number
- * @s2: second number
- *
- * Return: pointer to result string
- */
-char *multiply(char *s1, char *s2)
-{
-	int len1, len2, len, i, j, carry, digit1, digit2;
-	char *result;
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2;
-	result = malloc(sizeof(char) * (len + 1));
-	if (result == NULL)
-		return (NULL);
-
-	/* Initialize result with zeros */
-	for (i = 0; i < len; i++)
-		result[i] = '0';
-	result[len] = '\0';
-
-	/* Multiply each digit */
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		digit1 = s1[i] - '0';
-
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			digit2 = s2[j] - '0';
-			carry += (result[i + j + 1] - '0') + (digit1 * digit2);
-			result[i + j + 1] = (carry % 10) + '0';
-			carry /= 10;
-		}
-
-		if (carry > 0)
-			result[i + j + 1] = (carry % 10) + '0';
-	}
-
-	return (result);
-}
-
-/**
- * remove_leading_zeros - removes leading zeros from result
- * @s: string to process
- *
- * Return: pointer to processed string
- */
-char *remove_leading_zeros(char *s)
-{
-	int i = 0;
-
-	while (s[i] == '0' && s[i + 1] != '\0')
-		i++;
-
-	return (s + i);
-}
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
 
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	char *num1, *num2, *result;
+    char *s1, *s2;
+    int len1, len2, len, i, carry, n1, n2, *result, a = 0;
 
-	/* Check argument count */
-	if (argc != 3)
-		errors();
-
-	/* Check if arguments are digits */
-	num1 = argv[1];
-	num2 = argv[2];
-
-	if (!is_digit(num1) || !is_digit(num2))
-		errors();
-
-	/* Multiply the numbers */
-	result = multiply(num1, num2);
-	if (result == NULL)
-		errors();
-
-	/* Remove leading zeros and print result */
-	result = remove_leading_zeros(result);
-	printf("%s\n", result);
-
-	free(result);
-	return (0);
+    if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+        errors();
+    s1 = argv[1], s2 = argv[2];
+    len1 = _strlen(s1);
+    len2 = _strlen(s2);
+    len = len1 + len2;
+    result = calloc(len, sizeof(int));
+    if (result == NULL)
+        return (1);
+    for (len1 -= 1; len1 >= 0; len1--)
+    {
+        n1 = s1[len1] - '0';
+        carry = 0;
+        for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+        {
+            n2 = s2[len2] - '0';
+            carry += result[len1 + len2 + 1] + (n1 * n2);
+            result[len1 + len2 + 1] = carry % 10;
+            carry /= 10;
+        }
+        if (carry > 0)
+            result[len1 + len2 + 1] += carry;
+    }
+    for (i = 0; i < len; i++)
+    {
+        if (result[i])
+            a = 1;
+        if (a)
+            _putchar(result[i] + '0');
+    }
+    if (!a)
+        _putchar('0');
+    _putchar('\n');
+    free(result);
+    return (0);
 }
+
+/**
+ * is_digit - checks if a string contains only digits
+ * @s: string
+ * Return: 1 if all digits, else 0
+ */
+int is_digit(char *s)
+{
+    int i = 0;
+    while (s[i])
+    {
+        if (s[i] < '0' || s[i] > '9')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+/**
+ * _strlen - returns string length
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
+        i++;
+    return (i);
+}
+
+/**
+ * errors - prints error and exits
+ */
+void errors(void)
+{
+    printf("Error\n");
+    exit(98);
+}
+
