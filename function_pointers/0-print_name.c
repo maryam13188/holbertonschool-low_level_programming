6430b16f62dad9b1e8968241c6d1027822ec6dd4
maryam13188 <roro13188@gmail.com>
2025-11-10 08:41:40 +0000

    Add function print_name - task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -8,10 +8,10 @@
  * Description: The function takes a name and a function pointer,
  *              then calls the function with the name as argument.
  */
+#include "function_pointers.h"
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+    if (name && f)
+        f(name);
 }
