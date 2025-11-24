4e46408ba2f2c0f0d072fa72a30fb8f9b12706c4
maryam13188 <roro13188@gmail.com>
2025-11-24 09:14:54 +0000

    feat: implement print_list function

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,19 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    const list_t *current = h;
+	size_t count = 0;
+	const list_t *current = h;
 
-    while (current != NULL)
-    {
-        if (current->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", current->len, current->str);
-        
-        count++;
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", current->len, current->str);
 
-    return (count);
+		count++;
+		current = current->next;
+	}
+
+	return (count);
 }
