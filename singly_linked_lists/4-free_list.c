e0446a261460b9228a0eb52c9b8133aa9b1ab211
maryam13188 <roro13188@gmail.com>
2025-11-24 11:20:38 +0000

    add

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,21 +1,29 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_list - frees a list_t list
- * @head: pointer to the head of the list
- */
+* free_list - frees a list_t list
+* @head: pointer to the head of the list
+*
+* Description: Frees all nodes of a linked list, including the strings
+*/
 void free_list(list_t *head)
 {
-	list_t *current, *next;
+list_t *current;
+list_t *next_node;
 
-	current = head;
+current = head;
 
-	while (current != NULL)
-	{
-		next = current->next;  /* Save next pointer before freeing */
-		free(current->str);    /* Free the duplicated string */
-		free(current);         /* Free the node itself */
-		current = next;        /* Move to next node */
-	}
+while (current != NULL)
+{
+next_node = current->next;
+
+/* Free the string if it exists */
+if (current->str != NULL)
+free(current->str);
+
+/* Free the node itself */
+free(current);
+
+current = next_node;
+}
 }
