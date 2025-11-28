4bba5bafd624b275b9f29abecf2d2e8c7904b41c
maryam13188 <roro13188@gmail.com>
2025-11-28 18:11:07 +0000

    add

---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file with specified content
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written = 0;
+	size_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* Open file with required flags and permissions */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	/* If text_content is not NULL, write it to the file */
+	if (text_content != NULL)
+	{
+		/* Calculate length of text_content */
+		while (text_content[len])
+			len++;
+
+		/* Write to file */
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || (size_t)bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
