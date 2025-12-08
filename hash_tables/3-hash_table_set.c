c87840299bb4670cbdc26cfa3543ae885ea2f548
maryam13188 <roro13188@gmail.com>
2025-12-08 04:34:55 +0000

    fix: Refactor hash_table_set for Betty compliance

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -7,65 +7,53 @@
  * hash_table_set - Add or update an element in a hash table
  * @ht: The hash table to add/update the key/value to
  * @key: The key (cannot be an empty string)
- * @value: The value associated with the key (must be duplicated)
+ * @value: The value associated with the key
  *
  * Return: 1 if successful, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *current;
-	unsigned long int index;
-	char *value_copy;
+	hash_node_t *new, *tmp;
+	unsigned long int idx;
+	char *val_copy;
 
-	/* Check for NULL pointers and empty key */
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (!ht || !key || !*key || !value)
 		return (0);
 
-	/* Create a copy of the value */
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	val_copy = strdup(value);
+	if (!val_copy)
 		return (0);
 
-	/* Get the index for the key */
-	index = key_index((const unsigned char *)key, ht->size);
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
 
-	/* Check if key already exists */
-	current = ht->array[index];
-	while (current != NULL)
+	while (tmp)
 	{
-		if (strcmp(current->key, key) == 0)
+		if (!strcmp(tmp->key, key))
 		{
-			/* Key exists, update the value */
-			free(current->value);
-			current->value = value_copy;
+			free(tmp->value);
+			tmp->value = val_copy;
 			return (1);
 		}
-		current = current->next;
+		tmp = tmp->next;
 	}
 
-	/* Create a new node */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
 	{
-		free(value_copy);
+		free(val_copy);
 		return (0);
 	}
-
-	/* Duplicate the key */
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	new->key = strdup(key);
+	if (!new->key)
 	{
-		free(new_node);
-		free(value_copy);
+		free(new);
+		free(val_copy);
 		return (0);
 	}
-
-	/* Set the node's values */
-	new_node->value = value_copy;
-	
-	/* Add the new node at the beginning of the list */
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	new->value = val_copy;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
 
 	return (1);
 }
