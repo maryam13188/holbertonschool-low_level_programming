b8df4e0deb8a6dcbe56f04cbb47f38b22523651c
maryam13188 <roro13188@gmail.com>
2025-12-08 06:17:49 +0000

    feat: Implement hash_table_print function

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table to print
+ *
+ * Description: Prints key/value pairs in the order they appear
+ *              in the array of the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int first = 1; /* Flag to track first element */
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
