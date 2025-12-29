ae32bbe1d509a62bdc495511c07de0e4faf92607
maryam13188 <roro13188@gmail.com>
2025-12-29 18:54:08 +0000

    add

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) return 1;
-    
+    /* تعريف جميع المتغيرات أولاً */
     unsigned int hash = 0x1505;
-    char *s = argv[1];
+    char *s;
+    int i;
+    
+    if (argc != 2)
+    {
+        printf("Usage: %s username\n", argv[0]);
+        return (1);
+    }
+    
+    s = argv[1];
     
     while (*s)
-        hash = (hash << 5) + hash + *s++;
+    {
+        hash = (hash << 5) + hash + *s;
+        s++;
+    }
     
     printf("%u\n", hash);
     
-    return 0;
+    return (0);
 }
