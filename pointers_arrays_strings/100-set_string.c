4fabf2577ef0a1b46d0b531b46472ed7497b27b2
maryam13188 <roro13188@gmail.com>
2025-12-29 17:32:08 +0000

    add

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -2,13 +2,12 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: double pointer to the string pointer to modify
- * @to: pointer to the string to set
+ * @s: double pointer to modify
+ * @to: pointer to set
  *
  * Return: void
  */
 void set_string(char **s, char *to)
 {
-	char **temp = s;
-	*temp = to;
+    *s = to;
 }
